09db6ee1d4376b0fa656a050b719ee73e7973fa1
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-11-06 23:55:07 +0000

    done with 4-01 python

---
+++ ICS3U-Unit4-01.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2022 Evgeny Vovk All rights reserved.
+#
+# Created by: Evgeny Vovk
+# Created on: November 2022
+# ICS3U-Unit4-01.py File,
+# learning while statement in python.
+
+ 
+def main():
+
+    # input and variables
+    input_number = input("Input a number to add all the whole numbers up to it: ")
+    counter = 0
+    answer = 0
+
+    # process and output
+    print("")
+    try:
+        input_number_asint = int(input_number)
+        while counter < input_number_asint:
+            counter += 1
+            answer += counter
+            print("{0}".format(counter), end ="")
+            if counter < input_number_asint:
+                print(" + ", end ="")
+        print(" = {0}".format(answer))
+    except ValueError:
+        print("Invalid input, Please try again following the requirements.")
+
+    print("\n\nDone.")
+
+
+if __name__ == "__main__":
+    main()
